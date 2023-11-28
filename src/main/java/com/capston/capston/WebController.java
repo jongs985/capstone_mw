b99f13689692ffcacf95932b8e9bdb90509484cc
jongs985 <pjs000821@naver.com>
2023-11-28 13:43:52 +0900

    test1

--- src/main/java/com/capston/capston/WebController.java
+++ src/main/java/com/capston/capston/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
     @RequestMapping("/")
     public String main(){
-
+        //qwer
         return"index";
     }
 }
