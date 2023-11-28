a8f05731349716720e614a42f70b509cf7151046
user <nostalgy6@gmail.com>
2023-11-28 13:28:29 +0900

    commit

---
+++ src/main/java/com/capston/capston/WebController.java
@@ -0,0 +1,13 @@
+package com.capston.capston;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+    @RequestMapping("/")
+    public String main(){
+
+        return"index";
+    }
+}
